86ba372a5243ead1253eb7f1b33e336d4ba3688d
totomac <thomas.macquart@gmail.com>
2015-10-17 19:40:16 +0200

    First version + Add routes file

---
+++ bonapp-api/routes/routes.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('hello world');
+});
+
+app.get('/recipes/:recipeId', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('hello world');
+});
+
+app.use(function(req, res, next) {
+	es.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'ohoh I dont find this ressource');
+});
+
+app.listen(8080);
